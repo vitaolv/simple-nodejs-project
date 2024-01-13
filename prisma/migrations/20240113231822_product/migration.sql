3aa7dd3d929c73405e83e598761557ae0dcaf661
vitaolv <vlveleda@gmail.com>
2024-01-13 20:28:11 -0300

    primeiro commit

---
+++ prisma/migrations/20240113231822_product/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "productCode" TEXT NOT NULL,
+    "productName" TEXT NOT NULL,
+    "productDescription" TEXT NOT NULL,
+    "productPrice" REAL NOT NULL
+);
