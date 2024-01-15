55d89d73a8406d1af18b3eb569198ae744245f1a
vitaolv <vlveleda@gmail.com>
2024-01-15 06:47:28 -0300

    Aplicação do estilo da lista

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import '../styles/footer.sass'
+
+export function FooterComponent() {
+    return (
+        <div className='footer'>
+            <p>© 2023 Victor Veleda. Todos os direitos reservados.</p>
+            <a href='https://vitaolv.github.io/portfolio-Victor-Veleda/'>Visite meu portfolio 🚀</a>
+        </div>
+    )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 import { HeaderComponent } from './components/Header.tsx'
+import { FooterComponent } from './components/Footer.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HeaderComponent />
     <App />
+    <FooterComponent />
   </React.StrictMode>,
 )
