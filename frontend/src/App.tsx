f91c9933d698dac050f825acde8fe74ecdba9e8a
vitaolv <vlveleda@gmail.com>
2024-01-15 05:25:03 -0300

    Aplicando Sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import './App.sass'
 import { ProductsListComponent } from './components/ProductsListComponent'
 import { FormComponent } from './components/FormComponent'
 function App() {
 
   return (
-    <>
+    <div className='AppBody'>
       <FormComponent />
       <ProductsListComponent />
-    </>
+    </div>
   )
 }
 
