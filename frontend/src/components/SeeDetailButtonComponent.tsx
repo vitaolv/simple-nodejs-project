6ac3bf70fbd784979087f52c65fd63ea43a68887
vitaolv <vlveleda@gmail.com>
2024-01-17 19:35:07 -0300

    Implementando método GET para exibir produto por ID na API de produtos (SeeDetailButton).

--- frontend/src/components/SeeDetailButtonComponent.tsx
+++ frontend/src/components/SeeDetailButtonComponent.tsx
@@ -1,12 +1,34 @@
+import axios from "axios";
 interface TypesButtonComponent {
+    readonly productId: string;
     readonly text: string;
     readonly classStyle: string;
     readonly srcImage: string;
 }
 
-export function SeeDetailButtonComponent({ text, classStyle, srcImage }: TypesButtonComponent) {
+
+export function SeeDetailButtonComponent({ text, classStyle, srcImage, productId }: TypesButtonComponent) {
+
+    const handleSeeDetailButtonClick = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/products/${productId}`,
+                {
+                    method: 'GET'
+                }
+            )
+
+            if (response.status === 200) {
+                alert("Certo!" + productId)
+            }
+        } catch (err: unknown) {
+            alert(err)
+        }
+    }
     return (
-        <button className={`${classStyle}`}>
+        <button
+            className={`${classStyle}`}
+            onClick={handleSeeDetailButtonClick}
+        >
             <img src={srcImage} alt={text} />
             {text}
         </button>
