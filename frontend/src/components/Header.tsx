8627902263c9109eb6e3b04b999d671fe60c2714
vitaolv <vlveleda@gmail.com>
2024-01-18 01:13:11 -0300

    Exibir uma mensagem "Não temos produtos cadastrados" na lista de produtos quando não houver produtos cadastrados no banco de dados.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ export function HeaderComponent() {
                 <ul id="navList">
                     <li><a href="/home">Início</a></li>
                     <li><div id='traçado' /></li>
-                    <li><a href="/cadastro-de-produto">Registrar o produto</a></li>
+                    <li><a href="/cadastro-de-produto">Cadastrar o produto</a></li>
                 </ul>
             </nav>
         </div>
