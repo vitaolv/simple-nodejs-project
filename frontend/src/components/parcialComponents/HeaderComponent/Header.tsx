592e49dcdeb645b5dbe8411840a1da25f6023351
vitaolv <vlveleda@gmail.com>
2024-01-29 20:19:32 -0300

    Adicionando link Login no componente Header.

--- frontend/src/components/parcialComponents/HeaderComponent/Header.tsx
+++ frontend/src/components/parcialComponents/HeaderComponent/Header.tsx
@@ -4,12 +4,21 @@ export function HeaderComponent() {
     return (
         <div className='header'>
             <img src="/logo.svg" alt="logo" />
-            <nav>
-                <ul id="navList">
+            <nav className='nav-content'>
+                <ul className="navList">
                     <li><a href="/home">Início</a></li>
                     <li><div id='traçado' /></li>
                     <li><a href="/cadastro-de-produto">Cadastrar o produto</a></li>
                 </ul>
+                <ul className="navList">
+                    <li>
+                        <a className="button-to-login" href="/login">
+                            <img src="/user.svg" alt="login" />
+                            Login
+                        </a>
+                    </li>
+                </ul>
+
             </nav>
         </div>
     )
