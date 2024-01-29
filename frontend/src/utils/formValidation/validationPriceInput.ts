7c718801f9a34d440beb37d4249eca169c535361
vitaolv <vlveleda@gmail.com>
2024-01-29 06:15:00 -0300

    Corrigindo a expressão do formato de preço.

--- frontend/src/utils/formValidation/validationPriceInput.ts
+++ frontend/src/utils/formValidation/validationPriceInput.ts
@@ -1,6 +1,6 @@
 export function formatPrice(value: string) {
     let formattedValue = value.replace(/\D/g, "");
     formattedValue = formattedValue.replace(/(\d)(\d{2})$/, "$1,$2"); 
-    formattedValue = formattedValue.replace(/(?=(\d{3})+(?!\d))/g, ".");
+    formattedValue = formattedValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
     return `R$ ${formattedValue}`;
 }
