fb03d2b382de000014f377f77f491c2c48b3527f
vitaolv <vlveleda@gmail.com>
2024-01-29 19:55:17 -0300

    Ajeitando o tamanho de fonte e aumento de controle nos inputs.

--- frontend/src/components/LoginFormComponent/LoginFormComponent.tsx
+++ frontend/src/components/LoginFormComponent/LoginFormComponent.tsx
@@ -7,11 +7,20 @@ export function LoginFormComponent() {
                 <img src="/user.svg" alt="user-login" />
                 <label>
                     Nome de usuário
-                    <input type='text' />
+                    <input
+                        type='text'
+                        placeholder="Escolha um nome de usuário"
+                        maxLength={20}
+                        minLength={3}
+                    />
                 </label>
                 <label>
                     Senha
-                    <input type='password' />
+                    <input
+                        type='password'
+                        placeholder="Escolha uma senha"
+                        minLength={6}
+                    />
                 </label>
             </div>
             <button className='buttonPrimary' type='submit'>
